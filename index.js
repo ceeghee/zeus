da23cfd781059364233bc5996037c162067181a6
ceeghee <goodluck.ekene@gmail.com>
2018-10-07 17:46:07 +0100

    Implementing Support For Getting Address Types

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Zeus = require('./src/zeus');
 
 var valid = Zeus.validate('1KFzzGtDdnq5hrwxXGjwVnKzRbvf8WVxck', 'BTC');
+var type = Zeus.getAddressType('1KFzzGtDdnq5hrwxXGjwVnKzRbvf8WVxck', 'BTC');
+console.log(type);
 if(valid)
     console.log('This is a valid address');
 else
